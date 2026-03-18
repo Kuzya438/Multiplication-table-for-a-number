7dcf4f7bcea72751e4bd0546a7091391188b1664
Ivan <ivankuznetsov454@yandex.ru>
2026-03-18 23:00:10 +0500

    Add files via upload

---
+++ Multiplication table for a number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	int var{};
+	
+	std::cout << "¬ведите целое число:" << std::endl;
+	std::cin >> var;
+
+	for (int i{1}; i <= 10; i++)
+		std::cout << var << " x " << i << " = " << var * i << std::endl;
+
+	return EXIT_SUCCESS;
+}
